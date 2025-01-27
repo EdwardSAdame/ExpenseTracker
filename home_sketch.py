c61056271391ffecc02ad8bc6f5ed3971ee4d1e1
ESTUDIANTE <ESTUDIANTE@BE453O224S16ING.unal.loc>
2025-01-27 07:51:32 -0500

    sketch of a house

---
+++ home_sketch.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Create the main application window
+root = tk.Tk()
+root.title("Sketch of a Home")
+root.geometry("400x400")
+
+
+
+# Run the Tkinter main loop
+root.mainloop()
