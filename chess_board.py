e788a8ce8cae23d698e966be9248bfb3f933c685
ESTUDIANTE <ESTUDIANTE@BE453O224S16ING.unal.loc>
2025-01-27 08:38:37 -0500

    Final chess board

--- chess_board.py
+++ chess_board.py
@@ -13,6 +13,24 @@ canvas_size = board_size*square_size
 canvas = tk.Canvas(root, width=canvas_size, height=canvas_size)
 canvas.pack()
 
+#Draw the chessboar
+for row in range(board_size):
+    for col in range(board_size):
+        #Determine the color of the squares
+        if (row + col) % 2 == 0:
+            color = "white"
+        else:
+            color ="black"
+
+        #Calculate the coordinates of the square
+        x1 = col * square_size
+        y1 = row * square_size
+
+        x2 = x1 + square_size
+        y2 = y1 + square_size
+
+        #Draw the square
+        canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="gray")
 
 # Run the Tkinter main loop
 root.mainloop()
